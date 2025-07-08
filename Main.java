fd0d6b710e1719442568392ae9592a4afc295fb0
n-saailesh <saailesh213@gmail.com>
2025-07-08 15:58:02 -0400

    main update

--- Main.java
+++ Main.java
@@ -6,9 +6,17 @@ public class Main {
     
 
     public static void main(String[] args) throws FileNotFoundException {
-    File file = new File("C:\\Users\\MyPC\\OneDrive\\VS Code Projects\\317project//"+"CourseFile.txt");
-    Scanner myReader = new Scanner(file);
+    File file1 = new File("C:\\Users\\MyPC\\OneDrive\\VS Code Projects\\317project//"+"CourseFile.txt");
+    File file2 = new File("C:\\Users\\MyPC\\OneDrive\\VS Code Projects\\317project//"+"NameFile.txt");
 
+    Scanner myReader1 = new Scanner(file1);
+    Scanner myReader2 = new Scanner(file2);
+
+
+    // while(myReader.hasNextLine()) {
+    //     String i = myReader.nextLine();
+    //     System.out.println(i);
+    // }
 
     while(myReader.hasNextLine()) {
         String i = myReader.nextLine();
